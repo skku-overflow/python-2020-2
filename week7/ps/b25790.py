7f35e4cd98bd2b7a79824a6de2d455ad6aeced0f
강동윤 <kdy1997.dev@gmail.com>
2021-02-14 16:09:21 +0900

    주석 추가

--- week7/ps/b25790.py
+++ week7/ps/b25790.py
@@ -1,18 +1,18 @@
 from functools import lru_cache
 
 
-# N = int(input())
-# scores = [int(input() for _ in range(N))]
+N = int(input())
+scores = [int(input()) for _ in range(N)]
 
-N = 6
-scores = [0, 10, 20, 15, 25, 10, 20]
-
-# @lru_cache(maxsize=None)
-# def calc(cur_score, cur_idx):
-#     pass
+# N = 6
+# scores = [10, 20, 15, 25, 10, 20]
 
 
-@lru_cache(maxsize=None)
+# 시작 지점의 점수는 0
+scores.insert(0, 0)
+
+
+@ lru_cache(maxsize=None)
 def calc_score(cur_idx, streak):
     """
     cur_idx: 시작점. 처음엔 0.
@@ -30,19 +30,25 @@ def calc_score(cur_idx, streak):
     if N == cur_idx:
         return score
 
-    # 연속된 계단을 밟는 경우
+    # 다음 계단을 밟아서 얻을 수 있는 점수를 저장하는 배열
     possible_values = [0]
+
+    # 연속된 계단을 3개 밟는 경우 제외
     if streak != 2:
+        # 1칸 올라가는 경우에는 인덱스는 1이 늘어나고, 밟은 연속된 계단의 개수 + 1
         n1 = calc_score(cur_idx + 1, streak + 1)
         possible_values.append(n1)
 
+    # 마지막 계단 안 밟은 경우 제외
     if cur_idx + 2 <= N:
+        # 2칸 올라가는 경우에는 인덱스는 2가 늘어나고, 밟은 연속된 계단의 개수는 1로 초기화.
         n2 = calc_score(cur_idx + 2, 1)
         possible_values.append(n2)
 
-    max_score = score + max(possible_values)
-    print(N - cur_idx, possible_values)
-    return max_score
+    # print(N - cur_idx, possible_values)
+    return score + max(possible_values)
 
 
+# 시작 지점의 인덱스: 0
+# 시작 지점은 계단으로 보지 않는다고 했으므로 시작 지점에서 streak는 0
 print(calc_score(0, 0))
