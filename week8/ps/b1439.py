f89c1296e270030304d4a9d7a1ada8a417eefb69
강동윤 <kdy1997.dev@gmail.com>
2021-02-21 15:28:07 +0900

    b1439

--- week8/ps/b1439.py
+++ week8/ps/b1439.py
@@ -1,24 +1,38 @@
+from functools import lru_cache
 
 # S = input()
 S = "0001100"
 
+# 01010101010
+# 00010101010 => 010101010
+# 010101010: 2자리 차이
 
+
+# 01011110010001010 === 01010101010
+# 01010101010 => 2번째 1을 뒤집으면 00010101010
+# 00010101010 === 010101010
+
+
+@lru_cache(maxsize=None)
 def count(state):
     """
-    state = 배열, 연속된 숫자의 개수 저장.
-    111000011 이면 state는 [3, 4, 2]
+    state: 그룹 갯수.
+    111000011 이면 state는 3
     """
-    if len(state) == 1:
+    if state == 1:
         return 0
-    if len(statem) == 2 or len(statem) == 3:
+    if state == 2 or state == 3:
         return 1
 
     vals = []
-    for i in range(len(state)):
-        new_val = state[i]
-        if i is not 0:
+    for i in range(state):
+        # 첫 그룹이나 마지막 그룹을 뒤집는 경우
+        if i == 0 or i == state-1:
+            vals.append(count(state-1))
+        else:
+            vals.append(count(state-2))
 
-        state[i]
+    return min(vals)
 
 
 def parse(s):
@@ -39,5 +53,3 @@ def parse(s):
 
 
 assert parse("1110011") == [3, 2, 2]
-
-assert count([3, 2, 2]) == 1
