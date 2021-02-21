1be2f249e5bfaa350eaea879196c3a832f07362f
강동윤 <kdy1997.dev@gmail.com>
2021-02-21 15:18:29 +0900

    접근이 지나치게 복잡

--- week8/ps/b1439.py
+++ week8/ps/b1439.py
@@ -8,6 +8,17 @@ def count(state):
     state = 배열, 연속된 숫자의 개수 저장.
     111000011 이면 state는 [3, 4, 2]
     """
+    if len(state) == 1:
+        return 0
+    if len(statem) == 2 or len(statem) == 3:
+        return 1
+
+    vals = []
+    for i in range(len(state)):
+        new_val = state[i]
+        if i is not 0:
+
+        state[i]
 
 
 def parse(s):
@@ -28,3 +39,5 @@ def parse(s):
 
 
 assert parse("1110011") == [3, 2, 2]
+
+assert count([3, 2, 2]) == 1
